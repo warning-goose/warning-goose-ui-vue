6e9cb8ba306cffe4cf6f8c44d774bc7e4f206588
Glenn Y. Rolland <glenux@glenux.net>
2020-01-31 00:43:44 +0100

    Add new translations

--- popup/src/lang/en.js
+++ popup/src/lang/en.js
@@ -1,43 +1,88 @@
 export default {
-    topics: {
-        header: {
-            title: "Hey, Warning Goose!",
-            subtitle: "What alerts you to %s regarding the use of your personal information?"
-        },
-        selector: {
-            topic_access: "Who will have the right to see them?",
-            topic_purpose_usage: "What will they be used for?",
-            topic_transfer_thirdparty: "To whom are they transmitted?",
-            topic_transfer_use: "For what purpose are they transmitted?",
-            topic_privacy_contact: "Who can I contact to ask my questions?",
-            topic_too_many_data: "I am asked too much for the services offered!",
-            topic_too_complicated: "What I'm being told is too complicated!",
-            topic_protection: "How are they protected?",
-            topic_tracking_option: "I can't handle cookies!",
-            topic_all_or_nothing: "If I don't give them all, I'm not entitled to anything!",
-            topic_retention_time: "How long will they be kept?",
-            topic_discrimination_fear: "I'm afraid they'll penalize me!",
-            topic_discrimination_information: "How they are used to make decisions about me",
-            topic_consent_purpose: "I said yes but it's not clear!",
-            topic_consent_by_force: "I'm being forced to accept things I don't want!"
-        },
-        footer: {
-            action: "Continue"
-        }
+  about: {
+    header: {
+      title: "About Warning Goose",
+      subtitle: ""
     },
-    wassups: {
-        header: {
-            title: "Oh yes, yes, but still!",
-            subtitle: "In what mood does that put you?"
-        },
-        selector: {
-            i_am_wondering: "Hmm... I wonder",
-            i_am_forced_to: "I am being forced",
-            i_am_angry: "I'm angry",
-            i_am_worried_about: "I'm afraid"
-        },
-        footer: {
-            action: "Continue"
-        }
+    content: `Raise an alert on sites that bother you, make you doubt or annoy you about the use of your personal data.
+
+This browser add-on aims to support the organization of collective actions of citizens on the General Data Protection Regularion (GDPR) compliance issues.
+
+These actions make it possible to file a complaint and claim damages for the damage suffered.
+
+We do not collect any data automatically: we only collect the URL of the site for which you wish to alert us, and your email for the time it takes to confirm your humanity.
+
+If you have any questions: https://warninggoose.net`,
+    footer: {
+      action: "Return to my statement"
     }
+  },
+  topics: {
+    header: {
+      title: "Hey, Warning Goose!",
+      subtitle: "What alerts you to %s regarding the use of your personal information?"
+    },
+    selector: {
+      topic_access: "Who will have the right to see them?",
+      topic_purpose_usage: "What will they be used for?",
+      topic_transfer_thirdparty: "To whom are they transmitted?",
+      topic_transfer_use: "For what purpose are they transmitted?",
+      topic_privacy_contact: "Who can I contact to ask my questions?",
+      topic_too_many_data: "I am asked too much for the services offered!",
+      topic_too_complicated: "What I'm being told is too complicated!",
+      topic_protection: "How are they protected?",
+      topic_tracking_option: "I can't handle cookies!",
+      topic_all_or_nothing: "If I don't give them all, I'm not entitled to anything!",
+      topic_retention_time: "How long will they be kept?",
+      topic_discrimination_fear: "I'm afraid they'll penalize me!",
+      topic_discrimination_information: "How they are used to make decisions about me",
+      topic_consent_purpose: "I said yes but it's not clear!",
+      topic_consent_by_force: "I'm being forced to accept things I don't want!"
+    },
+    footer: {
+      action: "Continue"
+    }
+  },
+  wassups: {
+    header: {
+      title: "Oh yes, but still!",
+      subtitle: "In what mood does that put you?"
+    },
+    selector: {
+      i_am_wondering: "Hmm... I wonder",
+      i_am_forced_to: "I am being forced",
+      i_am_angry: "I'm angry",
+      i_am_worried_about: "I'm afraid"
+    },
+    footer: {
+      action: "Continue"
+    }
+  },
+  offensive: {
+    header: {
+      title: "Hey, there are laws protecting you!",
+      subtitle: "Will you stand up for your rights?"
+    },
+    footer: {
+      action: "Continue"
+    }
+  },
+  human: {
+    header: {
+      title: "Prove to us that you're human.",
+      subtitle: "What's your email?"
+    },
+    footer: {
+      action: "Send"
+    }
+  },
+  finish: {
+    header: {
+      title: "Thanks Warning Goose",
+      subtitle: "See you soon for some citizen action!"
+    },
+    footer: {
+      action: "Finish"
+    }
+  },
 };
--- popup/src/lang/fr.js
+++ popup/src/lang/fr.js
@@ -1,43 +1,93 @@
 export default {
-    topics: {
-        header: {
-            title: "Hey, Warning Goose !",
-            subtitle: "Qu'est-ce qui t'alerte sur %s concernant l'utilisation de tes informations personnelles ?"
-        },
-        selector: {
-            topic_access: "Qui aura le droit de les voir ?",
-            topic_purpose_usage: "À quoi elles vont servir ?",
-            topic_transfer_thirdparty: "À qui elles sont transmises ?",
-            topic_transfer_use: "Pour quel usage sont-elles transmises ?",
-            topic_privacy_contact: "Qui je peux contacter pour poser mes questions ?",
-            topic_too_many_data: "On m'en demande trop par rapport aux services offerts !",
-            topic_too_complicated: "Ce que l'on m'explique est trop compliqué !",
-            topic_protection: "Comment sont-elles protégées ?",
-            topic_tracking_option: "Je ne peux pas gérer les cookies !",
-            topic_all_or_nothing: "Si je les donne pas toutes, j'ai le droit à rien !",
-            topic_retention_time: "Combien de temps seront-elles gardées ?",
-            topic_discrimination_fear: "J'ai peur qu'elles me pénalisent !",
-            topic_discrimination_information: "Comment elles sont utilisés pour prendre des décisions à mon sujet",
-            topic_consent_purpose: "J'ai dit oui mais c'est pas clair !",
-            topic_consent_by_force: "On me force à accepter des choses que je ne veux pas !"
-        },
-        footer: {
-            action: "Continuer"
-        }
+  about: {
+    header: {
+      title: "A propos de Warning Goose",
+      subtitle: ""
     },
-    wassup: {
-        header: {
-            title: "Ah oui, quand même !",
-            subtitle: "Dans quel état cela te met ?"
-        },
-        selector: {
-            i_am_wondering: "Hmm.. je m'interroge",
-            i_am_forced_to: "On m'oblige",
-            i_am_angry: "Je suis en colère",
-            i_am_worried_about: "J'ai peur"
-        },
-        footer: {
-            action: "Continuer"
-        }
+    content: `Levez une alerte sur les sites qui vous gênent, vous font douter ou vous énervent concernant l'usage de vos données personnelles .
+
+L'extension sert de support à l'organisation d'actions collectives 
+de citoyens sur les questions de conformité au Règlement Général 
+sur la Protection des Données (RGPD).
+
+Ces actions permettent de porter plainte et demander des dommages 
+et intérêt pour les préjudices subis.
+
+Nous ne collectons aucune donnée de façon automatisée : seule est 
+recueillie l'URL du site pour lequel vous souhaitez nous alerter, 
+et votre email le temps d'une confirmation de votre humanité.
+
+Si vous avez des questions : https://warninggoose.net`,
+    footer: {
+      action: "Retour à ma déclaration"
     }
+  },
+  topics: {
+    header: {
+      title: "Hey, Warning Goose !",
+      subtitle: "Qu'est-ce qui t'alerte sur %s concernant l'utilisation de tes informations personnelles ?"
+    },
+    selector: {
+      topic_access: "Qui aura le droit de les voir ?",
+      topic_purpose_usage: "À quoi elles vont servir ?",
+      topic_transfer_thirdparty: "À qui elles sont transmises ?",
+      topic_transfer_use: "Pour quel usage sont-elles transmises ?",
+      topic_privacy_contact: "Qui je peux contacter pour poser mes questions ?",
+      topic_too_many_data: "On m'en demande trop par rapport aux services offerts !",
+      topic_too_complicated: "Ce que l'on m'explique est trop compliqué !",
+      topic_protection: "Comment sont-elles protégées ?",
+      topic_tracking_option: "Je ne peux pas gérer les cookies !",
+      topic_all_or_nothing: "Si je les donne pas toutes, j'ai le droit à rien !",
+      topic_retention_time: "Combien de temps seront-elles gardées ?",
+      topic_discrimination_fear: "J'ai peur qu'elles me pénalisent !",
+      topic_discrimination_information: "Comment elles sont utilisés pour prendre des décisions à mon sujet",
+      topic_consent_purpose: "J'ai dit oui mais c'est pas clair !",
+      topic_consent_by_force: "On me force à accepter des choses que je ne veux pas !"
+    },
+    footer: {
+      action: "Continuer"
+    }
+  },
+  wassup: {
+    header: {
+      title: "Ah oui, quand même !",
+      subtitle: "Dans quel état cela te met ?"
+    },
+    selector: {
+      i_am_wondering: "Hmm.. je m'interroge",
+      i_am_forced_to: "On m'oblige",
+      i_am_angry: "Je suis en colère",
+      i_am_worried_about: "J'ai peur"
+    },
+    footer: {
+      action: "Continuer"
+    }
+  },
+  offensive: {
+    header: {
+      title: "Hey, des lois te protègent !",
+      subtitle: "Veux-tu défendre tes droits ?"
+    },
+    footer: {
+      action: "Continuer"
+    }
+  },
+  human: {
+    header: {
+      title: "Prouve-nous que tu es humain !",
+      subtitle: "Quel est ton email ?"
+    },
+    footer: {
+      action: "Envoyer"
+    }
+  },
+  finish: {
+    header: {
+      title: "Merci Warning Goose",
+      subtitle: "À bientôt pour une action citoyenne !"
+    },
+    footer: {
+      action: "Terminer"
+    }
+  },
 };
